96cc1f1d4d2c8cb90cdaf1d0e1d9404759886360
Nolan O'Connor <nolan.oconnor55@gmail.com>
2023-09-17 21:39:04 -0400

    update

--- Portfolio_Game.py
+++ Portfolio_Game.py
@@ -3,6 +3,7 @@
 #   Make tomadachi game where I am the little developer in a room you can interact with
 #   You can give him compliments or thumbs up, which are "tracked" by the site and displayed
 #   You can offer him a job or send him a message both of which open up a message window to say something
+#   Just simele things to grab attention
 ###
 
 print("hi")
